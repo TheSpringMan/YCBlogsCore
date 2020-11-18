30f042aa49c0ba69e6358ffaf07b4a57359a5a33
TheSpringMan <springycli@163.com>
2020-11-18 15:12:11 +0800

    添加RoleRepository

--- YCBlogsCore.Data/YCBlogsCore.Data.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace YCBlogsCore.Data.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ YCBlogsCore.Data/YCBlogsCore.Data.Repository/RoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YCBlogsCore.Data;
+using YCBlogsCore.Data.EF;
+using YCBlogsCore.Domain.Models;
+
+namespace YCBlogsCore.Data.Repository
+{
+    public interface IRoleRepository:IRepository<Role, Int32>
+    {
+
+    }
+    public class RoleRepository : Repository<Role, Int32>, IRoleRepository
+    {
+        public RoleRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
